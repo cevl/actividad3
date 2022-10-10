606481e0ef4d3d79d650d945fc2f7db274dee7a7
cesar_vasquez <72132606+cevl@users.noreply.github.com>
2022-10-10 10:37:25 +0000

    Added table and modified table to fetch from.

--- actividad3.php
+++ actividad3.php
@@ -3,11 +3,27 @@
     $password = "1162331";
     $ODBCConnection = odbc_connect("DRIVER={Devart ODBC Driver for PostgreSQL};Server=db;Database=cesar_vasquez;Port=5432;String Types=Unicode", $user, $password);
 
-    $SQLQuery = "SELECT * FROM autotest.dept";
+    $SQLQuery = "SELECT * FROM Ejemplo";
     $RecordSet = odbc_exec($ODBCConnection, $SQLQuery);
-
+?>
+<table class="table table-bordered">
+    <thead><tr><th>S.N</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Matricula</th>
+    </thead>
+    <tbody>
+<?php
    while (odbc_fetch_row($RecordSet)) {
         $result = odbc_result_all($RecordSet, "border=1");
+?>
+         <tr>
+             <td><?php echo $sn; ?></td>
+             <td><?php echo $data['nombre']??''; ?></td>
+             <td><?php echo $data['apellido']??''; ?></td>
+             <td><?php echo $data['matricula']??''; ?></td>
+        </tr>
+<?php
     }
     odbc_close($ODBCConnection);
 ?>  
